63ebf23d9599793d10326398c612c5b76a7c5de7
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2020-04-17 12:04:38 -0600

    Add monitor for 4 stave setup at LANL

---
+++ online/mvtx2020_pmon/mvtxOM.h
@@ -0,0 +1,19 @@
+#ifndef __MVTX_OM_H__
+#define __MVTX_OM_H__
+
+#include <Event/Event.h>
+
+int process_event (Event *e); //++CINT
+int process_histos(float thresh = 10);
+int mask_pixels(float thresh = 5);
+int analysis();
+int OM();
+int print_canvas();
+int print_status();
+void reset_histos();
+void set_verbose(int v);
+void set_refresh(int r);
+void set_mask_hit_file_path(const char* aPath);
+void get_alignment();
+
+#endif /* __MVTX_OM_H__ */
