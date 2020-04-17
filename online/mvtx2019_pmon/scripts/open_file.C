aad94026426f1f5d41475406f3312f1f4dfe60f3
ycorrales <yasser.corrales@gmail.com>
2020-04-17 12:08:12 -0600

    change file path to local data store

--- online/mvtx2019_pmon/scripts/open_file.C
+++ online/mvtx2019_pmon/scripts/open_file.C
@@ -14,7 +14,8 @@ void open_file(int runnum, int nevents = 10001, const char* ftype = "longrun")
   //int runnum=atoi(filename.Data());
   //sprintf(filein,"../beamtest2019/%s/%s_%08d-0000.prdf",ftype,ftype,runnum);
   //sprintf(filein,"/mnt/databkup_1/MVTX_testbeam2019/%s/%s_%08d-0000.prdf",ftype,ftype,runnum);
-  sprintf(filein,"/sphenix/data/data01/mvtx/%s/%s_%08d-0000.prdf",ftype,ftype,runnum);
+  //  sprintf(filein,"/sphenix/data/data01/mvtx/%s/%s_%08d-0000.prdf",ftype,ftype,runnum);
+  sprintf(filein,"/data/MVTX_testbeam2019/%s/%s_%08d-0000.prdf",ftype,ftype,runnum);
   pfileopen(filein);
   prun(nevents);
   OM();
